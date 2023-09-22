7813cdb30642ac1a94efb3129f04c0b08fe3804b
NazarSadovij <145208436+NazarSadovij@users.noreply.github.com>
2023-09-22 21:10:37 +0300

    Add files via upload

--- Homework1.py
+++ Homework1.py
@@ -1,3 +1,21 @@
 print ("Вітаю!")
 print ("Ця програма стане твоїм другом")
-print ("Вона вміє рахувати і загадувати тобі загадки")
+print ("Вона вміє рахувати і загадувати загадки")
+a1 = input ("Вибери варіант 1)Калькулятор 2)Загадки")
+if a1 == "1":
+    print ("Вітаю у calculator")
+    a = input ("Введи перше число")
+    b = input ("Введи друге число")
+    a = int (a)
+    b = int (b)
+    operation = input ("Вибери дію + - * /: ")
+    if operation == "+":
+        print (a + b)
+    if operation == "-":
+        print (a - b)
+    if operation == "*":
+        print (a * b)
+    if operation == "/":
+        print (a / b)
+elif a1 == "2":
+    print ("Я буду задавати тобі загадки")
